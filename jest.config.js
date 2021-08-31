3be2ef0eb50998059827377c16bcaa7ad7420153
Hellshi <hcslimaa@gmail.com>
2021-08-31 11:44:41 -0300

    build: added Jest and its dependencies to the project

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  modulePaths: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  //preset: '@shelf/jest-mongodb',
+  transform: { '.+\\.ts$': 'ts-jest' },
+}
---
+++ src/presentation/sing-up.spec.ts
@@ -0,0 +1,6 @@
+describe('', () => {
+  test('should sum 2', () => {
+    const result = 1 + 1
+    expect(result).toBe(2)
+  })
+})
