22e7cb9eed1f51adede5b057bf79f04396d590e5
Hellshi <hcslimaa@gmail.com>
2021-08-31 12:45:01 -0300

    refactor: changed if sentences for a loop to verify required fields

--- src/presentation/sing-up.ts
+++ src/presentation/sing-up.ts
@@ -3,28 +3,14 @@ import { HttpResponse, HttRequest } from './protocols/htpp'
 
 export class SingUpController implements Controller {
   public handle = (httpResquest: HttRequest): HttpResponse => {
-    if (!httpResquest.body.name) {
-      return {
-        body: 'Missing name',
-        statusCode: 400,
-      }
-    }
-    if (!httpResquest.body.email) {
-      return {
-        body: 'missing email',
-        statusCode: 400,
-      }
-    }
-    if (!httpResquest.body.password) {
-      return {
-        body: 'missing password',
-        statusCode: 400,
-      }
-    }
-    if (!httpResquest.body.passwordConfirmation) {
-      return {
-        body: 'missing confirmation',
-        statusCode: 400,
+    const requiredFields = ['name', 'email', 'passwordConfirm', 'password']
+
+    for (const field of requiredFields) {
+      if (!httpResquest.body[field]) {
+        return {
+          statusCode: 400,
+          body: '',
+        }
       }
     }
     return {
