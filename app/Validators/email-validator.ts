bd70921bbefc48a0db9c9352a51472a112d11e60
Hellshi <hcslimaa@gmail.com>
2021-09-01 12:50:50 -0300

    refactor: refactoring validator to respond with true or false

--- app/Validators/email-validator.ts
+++ app/Validators/email-validator.ts
@@ -1,12 +1,17 @@
 import { validator, schema, rules } from '@ioc:Adonis/Core/Validator'
 
 export const validateMail = async (email: string) => {
-  await validator.validate({
-    schema: schema.create({
-      email: schema.string({ trim: true }, [rules.email()]),
-    }),
-    data: {
-      email: email,
-    },
-  })
+  try {
+    await validator.validate({
+      schema: schema.create({
+        email: schema.string({ trim: true }, [rules.email()]),
+      }),
+      data: {
+        email: email,
+      },
+    })
+    return true
+  } catch (err) {
+    return false
+  }
 }
